f0d98892c34f066c0a792dd29d90af3b70dd1cf2
IdanGrady <idangrady@gmail.com>
2022-10-24 13:14:26 +0200

    Added MAtched FIlter

--- Lab2/Lab 2/MatchedFilter.py
+++ Lab2/Lab 2/MatchedFilter.py
@@ -1,27 +1,58 @@
 import numpy as np
 import scipy.signal as signal
+import matplotlib.pyplot as plt
 
 
+def corr(a,b):
+    zeros_add = np.zeros(len(b))
+    embedded_a= np.concatenate((zeros_add, a,zeros_add), axis = 0)
+    output = np.zeros(len(a)+ len(b)-1)
+    for i in range(len(output)):
+        for j in range(len(b)):
+            val1= embedded_a[i+j]
+            val2 = b[j]
+            output[i] += embedded_a[i+j]* b[j]
+
+    return output
 
 def conv(a,b):
-    # zeros_add = np.zeros(len(b))
-    # embedded_a = np.array([zeros_add, a,zeros_add])
-    # for i in range(len(a)):
-    #     for j in range(len(b)):
-    #
-    return np.convolve(a,b)
+    zeros_add = np.zeros(len(b))
+    embedded_a= np.concatenate((zeros_add, a,zeros_add), axis = 0)
+    output = np.zeros(len(a)+ len(b)-1)
+    #TODO: add flipping!
+    for i in range(len(output)):
+        for j in range(len(b)):
+            val1= embedded_a[i+j]
+            val2 = b[j]
+            output[i] += embedded_a[i+j]* b[j]
+
+    return output
 
 
-random_bit = np.where(np.random.normal(size = (20))<0,0,1)
-GWNS =np.random.normal(size=(random_bit.shape))
+def generate_seq(input, num =3):
+    arr = np.array([input for _ in range(num)])
 
-recieved_input = GWNS+ random_bit
+    return arr.flatten()
+
+add_noise = True
+
+random_bit = np.where(np.random.normal(size = (100))<0,0,1)
+generated_sqeuence = generate_seq(random_bit)
+
+GWNS =np.random.normal(size=(generated_sqeuence.shape)) if add_noise else np.zeros((generated_sqeuence.shape))
+
+
+recieved_input = GWNS+ generated_sqeuence
 
 filter = np.flip(np.array([0,0,1,0,0]))
 print(f"random_bit: {random_bit}")
-print(f"GWNS+ random_bit : {GWNS+ random_bit}")
+print(f"GWNS+ random_bit : {GWNS+ generated_sqeuence}")
 
 print(f"reverse: {filter}")
+convold = conv(recieved_input, random_bit)
+print(f"convolve : {convold}")
 
-print(f"convolve : {conv(recieved_input, filter)}")
+plt.bar(np.arange(0,len(convold)),convold/convold.max())
 
+
+plt.show()
