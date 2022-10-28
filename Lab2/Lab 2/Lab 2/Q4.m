943f1a2bdd7c9a6582c032244b0cf736eaec9911
IdanGrady <idangrady@gmail.com>
2022-10-28 12:07:40 +0200

    LAB2

--- Lab2/Lab 2/Lab 2/Q4.m
+++ Lab2/Lab 2/Lab 2/Q4.m
@@ -10,23 +10,17 @@ Ts = 1e-3; % -- given, sampling time interval
 slow_time = (1:size(r.Data_out)*Ts); % -- given 
 
 % make you plot here with imagesc 
-show = abs(data(slow_time));
+show = abs(data);
 
 
 %surf(10*log10(abs(data)))
 %hold on
-imagesc(abs(10*log10(data(slow_time,:))));
+imagesc(abs(10*log10(data)));
 
 
 figure()
 % === 3. RDM === 
 N_doppler = 512; % -- this line is given 
 freq = (-500:1000/(N_doppler+1):500);  % -- this line is given
-fft_data = 10*log10(abs(fft(data(1:N_doppler+2,:))));
-
-stem(trans(freq),fft_data(1:N_doppler+1,:))
-
-s = 3;
-% make your plot here 
-
-
+fft_data = fftshift(10*log10(abs(fft(data(1:N_doppler,:)))));
+imagesc(freq,range,fft_data)
